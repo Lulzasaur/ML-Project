8c16c283a934f03f768da6c3b708cdbca2cc68a5
Lulzasaur <frost9192@gmail.com>
2018-10-19 09:09:39 -0700

    got API to work, trying to figure out data shape

--- static/ml.js
+++ static/ml.js
@@ -2,46 +2,62 @@
 const $epochs = $('#epochs')
 const $ticker = $('#ticker')
 const $newForm = $("#new-form");
+const BASE_URL = 'https://api.iextrading.com/1.0'
 const model = tf.sequential()
 let epochs,
     ticker;
 
-model.add(tf.layers.conv1d({
-  inputShape: [2000, 10],
-  kernelSize: 100,
-  filters: 8,
-  strides: 2,
-  activation: 'relu',
-  kernelInitializer: 'VarianceScaling'
-}))
-
-model.add(tf.layers.conv1d({
-  inputShape: [2000, 10],
-  kernelSize: 100,
-  filters: 8,
-  strides: 2,
-  activation: 'relu',
-  kernelInitializer: 'VarianceScaling'
-}))
-
-model.add(tf.layers.maxPooling1d({
-  poolSize: [100],
-  strides: [2]
-}))
-
-model.add(tf.layers.dense({
-  units: 10,
-  kernelInitializer: 'VarianceScaling',
-  activation: 'softmax'
-}))
-
-model.compile({optimizer: 'sgd', loss: 'binaryCrossentropy', lr: 0.1})
-
-async function train(epo){
+async function train(epo,xs,ys){
   for(let i=0;i<epo;i++){
     const response = await model.fit(xs,ys)
     console.log(response.history.loss[0])
   }
+}
+
+async function getStockInfo(ticker,epochs){
+  let stockInfo = await $.get(`${BASE_URL}/stock/${ticker}/chart/6m`, function(response) {
+    let dates = response.map(r => Date.parse(r.date))
+    let prices = response.map(r => r.close)
+    let tdates = tf.tensor1d(dates)
+    let tprices = tf.tensor1d(prices)
+
+    tdates.print()
+    tprices.print()
+
+    model.add(tf.layers.conv1d({
+      inputShape: [dates.length, 1],
+      kernelSize: 10,
+      filters: 8,
+      strides: 2,
+      activation: 'relu',
+      kernelInitializer: 'VarianceScaling'
+    }))
+    
+    model.add(tf.layers.conv1d({
+      inputShape: [dates.length, 1],
+      kernelSize: 10,
+      filters: 8,
+      strides: 2,
+      activation: 'relu',
+      kernelInitializer: 'VarianceScaling'
+    }))
+    
+    model.add(tf.layers.maxPooling1d({
+      poolSize: [23],
+      strides: [2]
+    }))
+
+    model.add(tf.layers.dense({
+      units: 1,
+      kernelInitializer: 'VarianceScaling',
+      activation: 'softmax'
+    }))
+    
+    model.compile({optimizer: 'sgd', loss: 'binaryCrossentropy', lr: 0.1})
+    
+    train(epochs,tdates,tprices)
+  });
+
 }
 
 $newForm.on("submit", function(e) {
@@ -50,12 +66,7 @@ $newForm.on("submit", function(e) {
   ticker = $ticker.val()
   epochs = $epochs.val()
 
-
-  train(epochs).then(()=>{
-    console.log('training complete');
-    let outputs = model.predict(future_dates);
-    outputs.print();
-  })
+  getStockInfo(ticker,epochs)
 
 })
 
